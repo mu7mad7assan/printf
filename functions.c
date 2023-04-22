6ca947218b055ad1dd4814cfd4be9c36f945e2dc
mu7mad7assan <mu7mad.7assan@gmail.com>
2023-04-22 04:36:32 -0700

    modification

--- functions.c
+++ functions.c
@@ -170,8 +170,8 @@ int print_binary(va_list types, char buffer[],
 	a[0] = number / max_number;
 	for (i = 1; i < 32; i++)
 	{
-		m /= 2;
-		a[i] = (n / m) % 2;
+		max_number /= 2;
+		a[i] = (number / max_number) % 2;
 	}
 	for (i = 0, sum = 0, counter = 0; i < 32; i++)
 	{
